02f0de7384ebdcda0660fdfc4a45390bf02729f8
kenan <kenan@LAPTOP-5KTTCAE1>
2020-10-09 08:12:17 +0200

    changed in program

--- GitHubDemo/Program.cs
+++ GitHubDemo/Program.cs
@@ -11,6 +11,33 @@ namespace GitHubDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub");
+            ConsoleKeyInfo key = Console.ReadKey();
+            //Console.WriteLine(key.Key.ToString());
+
+            if (key.Key.ToString() == "Enter")
+            {
+                Console.WriteLine("you pressed enter");
+            }
+            if (key.Key.ToString() == "Escape")
+            {
+                Console.WriteLine("you pressed escape");
+            }
+            if (key.Key.ToString() == "SpaceBar")
+            {
+                Console.WriteLine("you pressed spacebar");
+            }
+            if (key.Key.ToString() == "UpArrow")
+            {
+                Console.WriteLine("you pressed uparrow");
+            }
+            if (key.Key.ToString() == "DownArrow")
+            {
+                Console.WriteLine("you pressed Downarrow");
+            }
+            if (key.Key.ToString() == "LeftArrow")
+            {
+                Console.WriteLine("you pressed Leftarrow");
+            }
         }
     }
 }
